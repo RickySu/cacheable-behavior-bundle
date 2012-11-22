d71fa488ce044b150aa4b1c7def7df9a78c14171
Ricky Su <ricky@ez2.us>
2012-11-22 13:32:45 +0800

    add one to many relation test with multiple key

--- Tests/ObjectBuilderModifierOneToManyTest.php
+++ Tests/ObjectBuilderModifierOneToManyTest.php
@@ -69,165 +69,258 @@ class ObjectBuilderModifierTest extends Base {
         $Object1 = new $ObjectClass1();
         $Object1->fromArray($OriginData['Object1']);
         $Object1->save();
-        
+
         foreach ($OriginData['Object2s'] as $OriginalObject2) {
             $Object2 = new $ObjectClass2();
             $Object2->fromArray($OriginalObject2);
             $Object2->save();
         }
-        $Object2s = $Object1->getObjecttestOnetomany2s();
-        $CountObject2s = $Object1->countObjecttestOnetomany2s();
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
 
         foreach ($OriginData['Object2s'] as $Index => $OriginalObject2) {
-            $this->assertEquals($OriginalObject2, $Object2s[$Index]->toArray());            
+            $this->assertEquals($OriginalObject2, $Object2s[$Index]->toArray());
         }
         $this->assertFalse($Object2s instanceof MockContainer);
         $this->assertTrue($CountObject2s === count($OriginData['Object2s']));
-       
+
         // save object will clear reference cache but keep count cache.
         foreach ($ModifyData['Object2s'] as $ModifyDataObject2) {
             $Object2 = $QueryClass2::create()->findPk($ModifyDataObject2['Id']);
             $Object2->fromArray($ModifyDataObject2);
             $Object2->save();
-        }        
+        }
         $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
-        $Object2s = $Object1->getObjecttestOnetomany2s();
-        $CountObject2s = $Object1->countObjecttestOnetomany2s();
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
         $this->assertFalse($Object2s instanceof MockContainer);
         $this->assertTrue($CountObject2s instanceof MockContainer);
         foreach ($ModifyData['Object2s'] as $Index => $ModifyDataObject2) {
             $this->assertEquals($ModifyDataObject2, $Object2s[$Index]->toArray());
-        }        
+        }
         $this->assertEquals((int) (string) $CountObject2s, count($ModifyData['Object2s']));
 
-        
+
         // delete an object will clear all cache.
         foreach ($ModifyData['Object2s'] as $ModifyDataObject2) {
-            $Object2 = $QueryClass2::create()->findPk($ModifyDataObject2['Id']);            
+            $Object2 = $QueryClass2::create()->findPk($ModifyDataObject2['Id']);
             $Object2->delete();
-        }        
+        }
         $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
-        $Object2s = $Object1->getObjecttestOnetomany2s();
-        $CountObject2s = $Object1->countObjecttestOnetomany2s();
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
         $this->assertFalse($Object2s instanceof MockContainer);
         $this->assertFalse($CountObject2s instanceof MockContainer);
-        $this->assertEquals(count($Object2s),0);
-        $this->assertEquals($CountObject2s,0);
+        $this->assertEquals(count($Object2s), 0);
+        $this->assertEquals($CountObject2s, 0);
 
         // insert an object will clear all cache.        
         foreach ($ModifyData['Object2s'] as $ModifyDataObject2) {
             $Object2 = new $ObjectClass2();
             $Object2->fromArray($ModifyDataObject2);
             $Object2->save();
-        }        
+        }
         $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
-        $Object2s = $Object1->getObjecttestOnetomany2s();
-        $CountObject2s = $Object1->countObjecttestOnetomany2s();
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
         $this->assertFalse($Object2s instanceof MockContainer);
         $this->assertFalse($CountObject2s instanceof MockContainer);
         foreach ($ModifyData['Object2s'] as $Index => $ModifyDataObject2) {
             $this->assertEquals($ModifyDataObject2, $Object2s[$Index]->toArray());
-        }        
+        }
         $this->assertEquals((int) (string) $CountObject2s, count($ModifyData['Object2s']));
-        
+
         // with criteria no cache
-        $Object2s = $Object1->getObjecttestOnetomany2s();
-        $Object2=$Object2s[0];
+        $Object2s = $Object1->$getMethod();
+        $Object2 = $Object2s[0];
         $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
-        $Criteria=new \Criteria();
-        $Criteria->add($ObjectClass2Peer::ID,$Object2->getId());
-        $Criteria->add($ObjectClass2Peer::KEY,null,\Criteria::ISNOTNULL);
-        $Object2s = $Object1->getObjecttestOnetomany2s($Criteria);
-        $Criteria=new \Criteria();
-        $Criteria->add($ObjectClass2Peer::ID,$Object2->getId());
-        $Criteria->add($ObjectClass2Peer::KEY,null,\Criteria::ISNOTNULL);        
-        $CountObject2s = $Object1->countObjecttestOnetomany2s($Criteria);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $Criteria->add($ObjectClass2Peer::KEY, null, \Criteria::ISNOTNULL);
+        $Object2s = $Object1->$getMethod($Criteria);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $Criteria->add($ObjectClass2Peer::KEY, null, \Criteria::ISNOTNULL);
+        $CountObject2s = $Object1->$countMethod($Criteria);
         $this->assertFalse($Object2s instanceof MockContainer);
         $this->assertFalse($CountObject2s instanceof MockContainer);
-        $this->assertEquals($CountObject2s,1);
-        $this->assertEquals($Object2->toArray(),$Object2s[0]->toArray());
+        $this->assertEquals($CountObject2s, 1);
+        $this->assertEquals($Object2->toArray(), $Object2s[0]->toArray());
 
         // with criteria and cache hit
-        $Object2s = $Object1->getObjecttestOnetomany2s();
-        $Object2=$Object2s[0];
+        $Object2s = $Object1->$getMethod();
+        $Object2 = $Object2s[0];
         $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
-        $Criteria=new \Criteria();
-        $Criteria->add($ObjectClass2Peer::KEY,null,\Criteria::ISNOTNULL);
-        $Criteria->add($ObjectClass2Peer::ID,$Object2->getId());        
-        $Object2s = $Object1->getObjecttestOnetomany2s($Criteria);
-        $Criteria=new \Criteria();
-        $Criteria->add($ObjectClass2Peer::KEY,null,\Criteria::ISNOTNULL);
-        $Criteria->add($ObjectClass2Peer::ID,$Object2->getId());                
-        $CountObject2s = $Object1->countObjecttestOnetomany2s($Criteria);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::KEY, null, \Criteria::ISNOTNULL);
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $Object2s = $Object1->$getMethod($Criteria);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::KEY, null, \Criteria::ISNOTNULL);
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $CountObject2s = $Object1->$countMethod($Criteria);
         $this->assertTrue($Object2s instanceof MockContainer);
         $this->assertTrue($CountObject2s instanceof MockContainer);
-        $this->assertEquals((int) (string)$CountObject2s,1);
-        $this->assertEquals($Object2->toArray(),$Object2s[0]->toArray());
-        
+        $this->assertEquals((int) (string) $CountObject2s, 1);
+        $this->assertEquals($Object2->toArray(), $Object2s[0]->toArray());
     }
 
-    public function testOneToManyRelationCacheMultiple() {
-        return;
-        $this->simpleBuild('one_to_many_relation_multiple', "ObjecttestMultiple");
-        $Object1 = new \ObjecttestMultipleOnetomany1();
-        $Object1->setId(1);
-        $Object1->setKey1(1);
-        $Object1->setKey2(2);
+    public function DataProvider_OneToManyRelationMultiple() {
+        $this->simpleBuild('one_to_many_relation_multiple', "Objecttest");
+        $ClassName = '\\ObjecttestOnetomanyMultiple';
+        for ($i = 0; $i < 5; $i++) {
+            $OriginObject2s = array();
+            $ModifyObject2s = array();
+            for ($j = 0; $j < 5; $j++) {
+                $OriginObject2s[] = array(
+                    'Id' => $i * 10 + $j,
+                    'Key2' => $i + 10,
+                    'Key1' => $i + 20,
+                    'Value' => \rand(),
+                );
+                $ModifyObject2s[] = array(
+                    'Id' => $i * 10 + $j,
+                    'Key2' => $i + 10,
+                    'Key1' => $i + 20,
+                    'Value' => \rand(),
+                );
+            }
+            $Row[] = array(
+                'ClassName' => $ClassName,
+                'OriginData' => array(
+                    'Object1' => array(
+                        'Id' => $i,
+                        'Key1' => $i + 10,
+                        'Key2' => $i + 20,
+                        'Value' => \rand(),
+                    ),
+                    'Object2s' => $OriginObject2s,
+                ),
+                'ModifyData' => array(
+                    'Object1' => array(
+                        'Id' => $i,
+                        'Key1' => $i + 10,
+                        'Key2' => $i + 20,
+                        'Value' => \rand(),
+                    ),
+                    'Object2s' => $ModifyObject2s,
+                ),
+            );
+        }
+        return $Row;
+    }
+
+    /**
+     *
+     * @dataProvider DataProvider_OneToManyRelationMultiple
+     */
+    public function testCacheClearOneToManyRelationMultiple($ClassName, $OriginData, $ModifyData) {        
+        $ObjectClass1 = "{$ClassName}1";
+        $QueryClass1 = "{$ClassName}1Query";
+        $ObjectClass2 = "{$ClassName}2";
+        $ObjectClass2Peer = "{$ClassName}2Peer";
+        $QueryClass2 = "{$ClassName}2Query";
+        $getMethod = str_replace('\\', '', "get{$ClassName}2s");
+        $countMethod = str_replace('\\', '', "count{$ClassName}2s");
+
+        $Object1 = new $ObjectClass1();
+        $Object1->fromArray($OriginData['Object1']);
         $Object1->save();
-        $Object2 = new \ObjecttestMultipleOnetomany2();
-        $Object2->setId(1);
-        $Object2->setKey2(1);
-        $Object2->setKey1(2);
-        $Object2->save();
 
-        $Object2s = $Object1->getObjecttestMultipleOnetomany2s();
-        $CountObject2s = $Object1->countObjecttestMultipleOnetomany2s();
-        $this->assertEquals(count($Object2s), 1, 'count object1 get one to many object2 relative by key1 with no cache');
-        $this->assertInstanceOf('\\ObjecttestMultipleOnetomany2', $Object2s[0], 'object1 get one to many object2 relative by key1 with no cache');
-        $this->assertTrue($CountObject2s === 1, 'object1 get one to many object2 relative by key1 with no cache');
+        foreach ($OriginData['Object2s'] as $OriginalObject2) {
+            $Object2 = new $ObjectClass2();
+            $Object2->fromArray($OriginalObject2);
+            $Object2->save();
+        }
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
 
-        $Object1 = \ObjecttestMultipleOnetomany1Query::create()->findPk(1);
-        $Object2s = $Object1->getObjecttestMultipleOnetomany2s();
-        $this->assertEquals(count($Object2s), 1, 'count object1 get one to many object2 relative by key1 with no cache');
-        $this->assertTrue($Object2s instanceof MockContainer, 'object1 get one to many object2 relative by key1 with cache');
-        $this->assertInstanceOf('\\ObjecttestMultipleOnetomany2', $Object2s[0], 'object1 get one to many object2 relative by key1 with cache');
-        $this->assertTrue($Object1->countObjecttestMultipleOnetomany2s() instanceof MockContainer, 'object1 count one to many object2 relative by key1 with cache');
-        $this->assertEquals((int) (string) $Object1->countObjecttestMultipleOnetomany2s(), 1, 'object1 count one to many object2 relative by key1 with cache');
+        foreach ($OriginData['Object2s'] as $Index => $OriginalObject2) {
+            $this->assertEquals($OriginalObject2, $Object2s[$Index]->toArray());
+        }
+                
+        $this->assertFalse($Object2s instanceof MockContainer);
+        $this->assertTrue($CountObject2s === count($OriginData['Object2s']));
 
-        // save an object will clear reference cache but keep count cache.
-        $Object2->setValue(1);
-        $Object2->save();
-        $Object1 = \ObjecttestMultipleOnetomany1Query::create()->findPk(1);
-        $Object2s = $Object1->getObjecttestMultipleOnetomany2s();
-        $this->assertEquals(count($Object2s), 1, 'count object1 get one to many object2 relative by key1 with no cache');
-        $this->assertFalse($Object2s instanceof MockContainer, 'object1 get one to many object2 relative by key1 with cache');
-        $this->assertInstanceOf('\\ObjecttestMultipleOnetomany2', $Object2s[0], 'object1 get one to many object2 relative by key1 with no cache');
-        $this->assertTrue($Object1->countObjecttestMultipleOnetomany2s() instanceof MockContainer, 'object1 count one to many object2 relative by key1 with cache');
-        $this->assertEquals((int) (string) $Object1->countObjecttestMultipleOnetomany2s(), 1, 'object1 count one to many object2 relative by key1 with cache');
+        // save object will clear reference cache but keep count cache.
+        foreach ($ModifyData['Object2s'] as $ModifyDataObject2) {
+            $Object2 = $QueryClass2::create()->findPk($ModifyDataObject2['Id']);
+            $Object2->fromArray($ModifyDataObject2);
+            $Object2->save();
+        }
+        $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
+        $this->assertFalse($Object2s instanceof MockContainer);
+        $this->assertTrue($CountObject2s instanceof MockContainer);
+        foreach ($ModifyData['Object2s'] as $Index => $ModifyDataObject2) {
+            $this->assertEquals($ModifyDataObject2, $Object2s[$Index]->toArray());
+        }
+        $this->assertEquals((int) (string) $CountObject2s, count($ModifyData['Object2s']));
 
-        // insert an object will clear all cache.
-        $Object3 = new \ObjecttestMultipleOnetomany2();
-        $Object3->setId(2);
-        $Object3->setKey2(1);
-        $Object3->setKey1(2);
-        $Object3->save();
-        $Object1 = \ObjecttestMultipleOnetomany1Query::create()->findPk(1);
-        $Object2s = $Object1->getObjecttestMultipleOnetomany2s();
-        $CountObject2s = $Object1->countObjecttestMultipleOnetomany2s();
-        $this->assertEquals(count($Object2s), 2, 'count object1 get one to many object2 relative by key1 with no cache');
-        $this->assertFalse($Object2s instanceof MockContainer, 'object1 get one to many object2 relative by key1 with cache');
-        $this->assertInstanceOf('\\ObjecttestMultipleOnetomany2', $Object2s[0], 'object1 get one to many object2 relative by key1 with no cache');
-        $this->assertTrue($CountObject2s === 2, 'object1 get one to many object2 relative by key1 with no cache');
 
         // delete an object will clear all cache.
-        $Object3->delete();
-        $Object1 = \ObjecttestMultipleOnetomany1Query::create()->findPk(1);
-        $Object2s = $Object1->getObjecttestMultipleOnetomany2s();
-        $CountObject2s = $Object1->countObjecttestMultipleOnetomany2s();
-        $this->assertEquals(count($Object2s), 1, 'count object1 get one to many object2 relative by key1 with no cache');
-        $this->assertFalse($Object2s instanceof MockContainer, 'object1 get one to many object2 relative by key1 with cache');
-        $this->assertInstanceOf('\\ObjecttestMultipleOnetomany2', $Object2s[0], 'object1 get one to many object2 relative by key1 with no cache');
-        $this->assertTrue($CountObject2s === 1, 'object1 get one to many object2 relative by key1 with no cache');
+        foreach ($ModifyData['Object2s'] as $ModifyDataObject2) {
+            $Object2 = $QueryClass2::create()->findPk($ModifyDataObject2['Id']);
+            $Object2->delete();
+        }
+        $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
+        $this->assertFalse($Object2s instanceof MockContainer);
+        $this->assertFalse($CountObject2s instanceof MockContainer);
+        $this->assertEquals(count($Object2s), 0);
+        $this->assertEquals($CountObject2s, 0);
+
+        // insert an object will clear all cache.        
+        foreach ($ModifyData['Object2s'] as $ModifyDataObject2) {
+            $Object2 = new $ObjectClass2();
+            $Object2->fromArray($ModifyDataObject2);
+            $Object2->save();
+        }
+        $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
+        $Object2s = $Object1->$getMethod();
+        $CountObject2s = $Object1->$countMethod();
+        $this->assertFalse($Object2s instanceof MockContainer);
+        $this->assertFalse($CountObject2s instanceof MockContainer);
+        foreach ($ModifyData['Object2s'] as $Index => $ModifyDataObject2) {
+            $this->assertEquals($ModifyDataObject2, $Object2s[$Index]->toArray());
+        }
+        $this->assertEquals((int) (string) $CountObject2s, count($ModifyData['Object2s']));
+
+        // with criteria no cache
+        $Object2s = $Object1->$getMethod();
+        $Object2 = $Object2s[0];
+        $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $Criteria->add($ObjectClass2Peer::KEY1, null, \Criteria::ISNOTNULL);
+        $Object2s = $Object1->$getMethod($Criteria);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $Criteria->add($ObjectClass2Peer::KEY1, null, \Criteria::ISNOTNULL);
+        $CountObject2s = $Object1->$countMethod($Criteria);
+        $this->assertFalse($Object2s instanceof MockContainer);
+        $this->assertFalse($CountObject2s instanceof MockContainer);
+        $this->assertEquals($CountObject2s, 1);
+        $this->assertEquals($Object2->toArray(), $Object2s[0]->toArray());
+
+        // with criteria and cache hit
+        $Object2s = $Object1->$getMethod();
+        $Object2 = $Object2s[0];
+        $Object1 = $QueryClass1::create()->findPk($OriginData['Object1']['Id']);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::KEY1, null, \Criteria::ISNOTNULL);
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $Object2s = $Object1->$getMethod($Criteria);
+        $Criteria = new \Criteria();
+        $Criteria->add($ObjectClass2Peer::KEY1, null, \Criteria::ISNOTNULL);
+        $Criteria->add($ObjectClass2Peer::ID, $Object2->getId());
+        $CountObject2s = $Object1->$countMethod($Criteria);
+        $this->assertTrue($Object2s instanceof MockContainer);
+        $this->assertTrue($CountObject2s instanceof MockContainer);
+        $this->assertEquals((int) (string) $CountObject2s, 1);
+        $this->assertEquals($Object2->toArray(), $Object2s[0]->toArray());
     }
 
 }
